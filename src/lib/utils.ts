8cf7a413a249cb75f6332493e513110aaa3ffff0
Vilmar Junior <152644425+v-junior@users.noreply.github.com>
2026-01-11 18:14:54 -0300

    Implement extractWidgetData function
    Add extractWidgetData function to process widget data

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+export const extractWidgetData = (data: any) => {
+  if (!data) return [];
+  // Pega os valores da primeira chave do objeto 'data' (ex: br.com.bb...)
+  const keys = Object.keys(data);
+  if (keys.length > 0 && Array.isArray(data[keys[0]])) {
+    return data[keys[0]];
+  }
+  return [];
+};
